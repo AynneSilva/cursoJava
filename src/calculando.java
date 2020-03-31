7a6c9c0ada28c5213680d083f560d5027535cdfa
“Aynne <aynnesilva@outlook.com>
2020-03-31 02:21:23 -0300

    calculando aumento de salario

---
+++ src/calculando.java
@@ -0,0 +1,7 @@
+public class calculando {
+    public static void main(String[] args) {
+      var salary = 15;
+      var calculo = salary + (50/100);
+      System.out.println("O aumento do seu salario foi:"+ calculo);
+    }
+}
