c37bddfc1d5209d6d6c3232dfd1dc7f01c077ab6
“Aynne <aynnesilva@outlook.com>
2020-03-31 09:59:15 -0300

    par ou impar

---
+++ src/restoDaDivisao.java
@@ -0,0 +1,15 @@
+public class restoDaDivisao {
+    public static void main(String[] args) {
+       var number = 3;
+
+        if (number %1 == 1){
+        System.out.println("impar");
+
+        } else {
+            System.out.println("par");
+        }
+    }
+
+
+}
+
