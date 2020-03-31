74316e6950994f4c0241907d51c8e37b15718402
“Aynne <aynnesilva@outlook.com>
2020-03-31 09:13:49 -0300

    estrutura condicional composta

--- src/calculando.java
+++ src/calculando.java
@@ -1,7 +1,17 @@
 public class calculando {
     public static void main(String[] args) {
-      var salary = 15;
-      var calculo = salary + (50/100);
-      System.out.println("O aumento do seu salario foi:"+ calculo);
+        /* @ AynneSilva */
+      var salary = 1500.0;
+      var porcent = 30/100 ;
+
+      var calculo = salary * 0.03;
+      var resultado = calculo + salary;
+
+      System.out.println("Seu salario teve um aumento de "+ calculo +" E agora seu salario é "+ resultado);
+      calculo = salary * 0.05;
+
+      System.out.println(calculo);
+      calculo = salary * 0.06;
+      System.out.println(calculo);
     }
 }
--- src/estruturaDecControle.java
+++ src/estruturaDecControle.java
@@ -1,2 +1,15 @@
 public class estruturaDecControle {
+    /* @AynneSilva */
+    public static void main(String[] args) {
+        var valor = true;
+        if (valor) {
+            System.out.println("Dentro do if");
+
+        } else {
+            System.out.println("Dentro do Else");
+
+        }
+
+    }
+
 }
